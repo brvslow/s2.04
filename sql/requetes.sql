1ccbc400b2b9c206e2eccd385719038633ab4cba
baptiste <baptiste.wasson@gmail.com>
2023-04-13 21:47:30 +0200

    closes #2

--- sql/requetes.sql
+++ sql/requetes.sql
@@ -1,10 +1,9 @@
 -- Exercice 3
+--
 -- Q1
 -- Output: 15
-SELECT
-    count(*)
-FROM
-    information_schema.columns
+SELECT count(*)
+FROM information_schema.columns
 WHERE
     table_schema = USER
     AND table_name = 'import_athletes';
@@ -28,7 +27,7 @@ FROM import_athletes;
 -- Output: 12116
 SELECT count(medal)
 FROM import_athletes
-WHERE medal = 'Gold';
+WHEREn medal = 'Gold';
 
 -- Q6
 -- Output: 2
@@ -38,13 +37,13 @@ WHERE name LIKE 'Carl Lewis%';
 
 
 
-
 -- Exercice 5
+--
 -- Q1
 -- Output:
 SELECT
     r.nom_pays,
-    count(*) AS nb_participation
+    count(*) AS nb_participations
 FROM
     participe AS p,
     athlete AS a,
@@ -60,7 +59,7 @@ ORDER BY count(*) DESC;
 -- Output:
 SELECT
     r.nom_pays,
-    count(p.medaille) AS nb_medaille_Or
+    count(p.medaille) AS nb_medailles_Or
 FROM
     participe AS p,
     athlete AS a,
@@ -77,7 +76,7 @@ ORDER BY count(p.medaille) DESC;
 -- Output:
 SELECT
     r.nom_pays,
-    count(p.medaille) AS nb_medaille_pays
+    count(p.medaille) AS nb_medailles_pays
 FROM
     participe AS p,
     athlete AS a,
@@ -95,20 +94,22 @@ ORDER BY count(p.medaille) DESC;
 SELECT
     a.ano,
     a.nom,
-    count(p.medaille) AS nb_medaille_Athlete
+    count(p.medaille) AS nb_medailles_Or
 FROM
     athlete AS a
     JOIN participe AS p ON a.ano = p.ano
+WHERE p.medaille = 'Gold'
 GROUP BY
     a.ano,
     a.nom
-ORDER BY count(p.medaille) DESC;
+ORDER BY
+    count(p.medaille) DESC;
 
 -- Q5
 -- Output:
 SELECT
     r.nom_pays,
-    count(p.medaille) AS nb_medaille_Albertville
+    count(p.medaille) AS nb_medailles_Albertville
 FROM
     athlete AS a,
     participe AS p,
@@ -127,35 +128,31 @@ ORDER BY count(p.medaille) DESC;
 
 -- Q6
 -- Output:
-SELECT count(distinct a.ano)
+SELECT DISTINCT p.ano
 FROM
-    athlete AS a,
     participe AS p,
-    equipe AS eq,
-    equipe AS eq2,
     region AS r,
     region AS r2
 WHERE
-    a.ano = p.ano
-    AND eq.nom_equipe = a.equipe 
-    AND eq2.nom_equipe = a.equipe
-    AND eq.noc = r.noc 
-    AND eq2.noc = r2.noc
-    AND eq.nom_pays <> eq2.nom_pays;
+    p.noc = r.noc
+    AND p.nom_equipe = r.nom_equipe
+    AND p.noc = r2.noc
+    AND p.nom_equipe = r2.nom_equipe
+    AND r.nom_pays <> r2.nom_pays
+    AND r.nom_pays = 'France';
 
 -- Q7
 -- Output:
-
-
+-- Même requête que la Q6
 -- Q8
 -- Output:
 SELECT
     a.age,
-    count(p.medaille)
-FROM 
+    count(p.medaille) nb_medailles_Or
+FROM
     athlete AS a
     JOIN participe AS p ON a.ano = p.ano
-WHERE 
+WHERE
     p.medaille = 'Gold'
     AND a.age IS NOT NULL
 GROUP BY a.age
@@ -163,18 +160,75 @@ ORDER BY count(p.medaille) DESC;
 
 -- Q9
 -- Output:
-
+SELECT
+    e.nom_sport,
+    count(p.medaille) AS nb_médailles_plus_de_50ans
+FROM
+    epreuve AS e,
+    athlete AS a,
+    participe AS p
+WHERE
+    a.age >= 50
+    AND e.evenement = p.evenement
+    AND e.nom_sport = p.nom_sport
+    AND e.genre = p.genre
+    AND a.ano = p.ano
+GROUP BY e.nom_sport
+ORDER BY count(p.medaille) DESC;
 
 -- Q10
 -- Output:
-
+SELECT
+    ed.annee,
+    ed.saison,
+    count(ep.evenement) nb_epreuves
+FROM
+    edition AS ed,
+    epreuve AS ep,
+    participe AS p
+WHERE
+    ep.evenement = p.evenement
+    AND ep.nom_sport = p.nom_sport
+    AND ep.genre = p.genre
+    AND ed.annee = p.annee
+    AND ed.saison = p.saison
+GROUP BY
+    ed.annee,
+    ed.saison
+ORDER BY ed.annee ASC;
 
 -- Q11
 -- Output:
-
-
-
+SELECT
+    ed.annee,
+    count(p.medaille) AS nb_médailles_femme_en_été
+FROM
+    edition AS ed,
+    participe AS p,
+    athlete AS a
+WHERE
+    a.sexe = 'F'
+    AND ed.saison = 'Summer'
+    AND ed.annee = p.annee
+    AND ed.saison = p.saison
+    AND a.ano = p.ano
+GROUP BY ed.annee
+ORDER BY ed.annee ASC;
 
 
 
 -- Exercice 6
+--
+-- Sport : Basket
+-- Pays : USA
+--
+-- Requête n°1 : Liste des pays avec le nombre de villes organisatrices issues de ce pays
+
+
+-- Requête n°2 : Liste des événements féminins se déroulant dans une année paire
+
+
+-- Requête n°3 : Athlètes où le pays organisateur d'une édition est le même que le pays qu'il représente
+
+
+-- Requête n°4 : Athlètes classés du plus petit au plus grand
